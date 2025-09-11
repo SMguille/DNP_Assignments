a09cd9148e5fa49739a4fb2efc99a2d8176646d0
Guillermo Sánchez Martínez <guigames2126@gmail.com>
2025-09-11 09:20:34 +0200

    final adding domain model

--- Server/Entities/Post.cs
+++ Server/Entities/Post.cs
@@ -2,11 +2,11 @@ using System;
 
 namespace Entities;
 
-public class Post(int Id, string Title, string Body, int UserID, int SubForumId)
+public class Post(int Id, string Title, string Body, int UserId, int SubForumId)
 {
     public int Id { get; set; } = Id;
     public string Title { get; set; } = Title;
     public string Body { get; set; } = Body;
-    public int UserID { get; set; } = UserID;
+    public int UserId { get; set; } = UserId;
     public int SubForumId { get; set; } = SubForumId;
 }
--- Server/Entities/obj/Debug/net10.0/Entities.AssemblyInfo.cs
+++ Server/Entities/obj/Debug/net10.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b5b60e6ec07ed54cbc5d6f2b57f5c5d636c4ee3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b7560c2e3465fae1f7e490f8e0eacf10780737f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
